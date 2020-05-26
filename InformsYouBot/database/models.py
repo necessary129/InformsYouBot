176f52bd94150b63d3e02c8b6168a786475df22e
Shamil K <nessessery129@gmail.com>
2020-05-26 14:53:40 +0530

    Add LICENSE

--- InformsYouBot/database/models.py
+++ InformsYouBot/database/models.py
@@ -1,3 +1,19 @@
+#  Copyright (C) 2020 Shamil K Muhammed
+
+# This file is part of InformsYouBot.
+
+# InformsYouBot is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    InformsYouBot is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with InformsYouBot.  If not, see <http://www.gnu.org/licenses/>.
 from sqlalchemy import (
     MetaData,
     Column,
