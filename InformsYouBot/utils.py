fb00791fb5b22313b6fa9deed9c56ba1190efc89
Shamil K <nessessery129@gmail.com>
2020-05-30 14:51:38 +0530

    Change message url to include domain.
    Apparently it doesn't work in mobile apps.

--- InformsYouBot/utils.py
+++ InformsYouBot/utils.py
@@ -104,7 +104,7 @@ def get_an_instance():
 def message_url(recipient, subject, message):
     return "".join(
         (
-            "/message/compose/?to=",
+            "https://np.reddit.com/message/compose/?to=",
             quote(recipient),
             "&subject=",
             quote(subject),
